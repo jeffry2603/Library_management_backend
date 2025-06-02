2f77c4212dc52b2f87cf2f5b502ab4ba3c5a1c04
Jeffry Allan <108166134+jeffry2603@users.noreply.github.com>
2025-06-02 23:30:04 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,20 +1,16 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  if (!process.env.MONGODB_URI) {
-    throw new Error("MONGODB_URI environment variable not defined");
-  }
-
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "MERN_STACK_LIBRARY_MANAGEMENT_SYSTEM",
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000
+      serverSelectionTimeoutMS: 5000,
+      retryWrites: true
     });
-    console.log(`Connected to MongoDB at ${mongoose.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error("MongoDB connection error:", err.message);
+    console.error(`Database connection error: ${err.message}`);
     process.exit(1);
   }
 };
