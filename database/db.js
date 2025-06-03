25854835f8e0662dd49a481f74109890c0470d8d
Jeffry Allan <108166134+jeffry2603@users.noreply.github.com>
2025-06-03 09:15:14 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000,
       retryWrites: true
     });
