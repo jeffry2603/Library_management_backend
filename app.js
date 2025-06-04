839e5dbd96eacf5fff2e4d9b7f79f6a9c2c87d95
Jeffry Allan <108166134+jeffry2603@users.noreply.github.com>
2025-06-04 10:27:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
