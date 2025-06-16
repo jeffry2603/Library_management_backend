66d33a02720ae83dc7436ccc6568e44f88c0878c
Jeffry Allan <108166134+jeffry2603@users.noreply.github.com>
2025-06-16 16:35:27 +0530

    Update borrowControllers.js

--- controllers/borrowControllers.js
+++ controllers/borrowControllers.js
@@ -5,6 +5,55 @@ import { Book } from "../models/bookModel.js";
 import { User } from "../models/userModel.js";
 import { calculateFine } from "../utils/fineCalculator.js";
 
+export const borrowBook = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  const user = req.user;
+
+  const book = await Book.findById(id);
+  if (!book) {
+    return next(new ErrorHandler("Book not found.", 404));
+  }
+
+  if (book.quantity === 0) {
+    return next(new ErrorHandler("Book not available.", 400));
+  }
+
+  const isAlreadyBorrowed = user.borrowedBooks.find(
+    (b) => b.bookId.toString() === id && b.returned === false
+  );
+  if (isAlreadyBorrowed) {
+    return next(new ErrorHandler("You have already borrowed this book.", 400));
+  }
+
+  book.quantity -= 1;
+  book.availability = book.quantity > 0;
+  await book.save();
+
+  user.borrowedBooks.push({
+    bookId: book._id,
+    bookTitle: book.title,
+    borrowedDate: new Date(),
+    dueDate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  });
+  await user.save();
+
+  await Borrow.create({
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+    book: book._id,
+    dueDate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    price: book.price,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Book borrowed successfully!",
+  });
+});
+
 export const recordBorrowedBook = catchAsyncErrors(async (req, res, next) => {
   const { id } = req.params;
   const { email } = req.body;
@@ -93,7 +142,7 @@ export const returnBorrowBook = catchAsyncErrors(async (req, res, next) => {
     success: true,
     message:
       fine !== 0
-        ? `The book has been returned successfully. The total charges, including a fine, are $${
+        ? `The book has been returned successfully. The total charges, including a fine, are ${
             fine + book.price
           }`
         : `The book has been returned successfully. The total charges are $${book.price}`,
