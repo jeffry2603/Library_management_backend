e9696aa2c62051b74ed2453702ef22bb02c09ae9
Jeffry Allan <108166134+jeffry2603@users.noreply.github.com>
2025-06-17 14:16:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,24 +16,7 @@ export const app = express();
 
 config({ path: "./config/config.env" });
 
-const allowedOrigins = [
-  "https://newlibrarysystem.netlify.app",
-  "http://localhost:5173",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors())
 
 app.use(cookieParser());
 app.use(express.json());
