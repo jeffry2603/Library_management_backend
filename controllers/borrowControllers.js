18214d7b42a627a41df966eeabb5cd9d626dcb2f
Jeffry Allan <108166134+jeffry2603@users.noreply.github.com>
2025-06-17 11:14:45 +0530

    Update borrowControllers.js

--- controllers/borrowControllers.js
+++ controllers/borrowControllers.js
@@ -53,6 +53,55 @@ export const recordBorrowedBook = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
+export const userBorrowBook = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  const userId = req.user._id;
+  
+  const book = await Book.findById(id);
+  if (!book) {
+    return next(new ErrorHandler("Book not found.", 404));
+  }
+  
+  if (book.quantity === 0) {
+    return next(new ErrorHandler("Book not available.", 400));
+  }
+  
+  const isAlreadyBorrowed = req.user.borrowedBooks.find(
+    b => b.bookId.toString() === id && b.returned === false
+  );
+  if (isAlreadyBorrowed) {
+    return next(new ErrorHandler("You have already borrowed this book.", 400));
+  }
+  
+  book.quantity -= 1;
+  book.availability = book.quantity > 0;
+  await book.save();
+  
+  req.user.borrowedBooks.push({
+    bookId: book._id,
+    bookTitle: book.title,
+    borrowedDate: new Date(),
+    dueDate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  });
+  await req.user.save();
+  
+  await Borrow.create({
+    user: {
+      id: req.user._id,
+      name: req.user.name,
+      email: req.user.email,
+    },
+    book: book._id,
+    dueDate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    price: book.price,
+  });
+  
+  res.status(200).json({
+    success: true,
+    message: "Book borrowed successfully.",
+  });
+});
+
 export const returnBorrowBook = catchAsyncErrors(async (req, res, next) => {
   const { bookId } = req.params;
   const { email } = req.body;
